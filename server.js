693277d04923989686416f47f5186a1b9822f975
Chakali Hareesh <152854575+HariHareesh@users.noreply.github.com>
2026-05-11 20:37:08 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 const frontendPath = path.join(__dirname);
